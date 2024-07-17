39d748e897ae1cd5675249938fd467d78fc817a8
Ananym <32470867+Ananym@users.noreply.github.com>
2024-07-18 00:45:04 +0100

    Remove self-signed codesign from build step; fix .ps1 reference

--- build_dist.ps1
+++ build_dist.ps1
@@ -7,22 +7,7 @@ if(-not $?) {
 Copy-Item config.yaml dist/audiomixer/
 Copy-Item .\readme.md dist\audiomixer\readme.md
 Copy-Item .\LICENCE dist\audiomixer\LICENCE
-Copy-Item ".\add-to-startup.ps1" ".\dist\audiomixer\add-to-startup.ps1"
-$pw = Read-Host "Cert pw: " -AsSecureString
-$pwText = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pw))
-signtool sign `
-/f code_signing_csr.txt `
-/p $pwText `
-/fd sha256 `
-/tr http://timestamp.digicert.com `
-/td sha256 `
-/d "Windows utility for controlling per-program volumes" `
-./dist/audiomixer/audiomixer.exe
-
-if (-not $?) {
-    Write-Host "Failed to sign"
-    exit 1
-}
+Copy-Item ".\add-exe-to-startup.ps1" ".\dist\audiomixer\add-exe-to-startup.ps1"
 
 Remove-Item ./dist/release.zip
 
